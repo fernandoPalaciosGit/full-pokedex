a3725894068ef5e371897f97db4416d32f3840a4
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-09-23 13:38:15 +0200

    create template directives to pokemon properties

--- app/pokedex/directives/partials/pokedex-partials.js
+++ app/pokedex/directives/partials/pokedex-partials.js
@@ -2,10 +2,38 @@
 
 	angular.module('app-pokemon.pokedex.partial-directives', [])
 
-	.directive('pokedexTmpl', [function(){
+	.directive('pokedexData', [function(){
 		return {
 			restrict: 'E',
-			templateUrl: 'pokedex/directives/partials/pokedex.tmpl.html'
+			templateUrl: 'pokedex/directives/partials/data.tmpl.html'
+		};
+	}])
+
+	.directive('pokedexStats', [function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'pokedex/directives/partials/stats.tmpl.html'
+		};
+	}])
+
+	.directive('pokedexEvolution', [function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'pokedex/directives/partials/evol.tmpl.html'
+		};
+	}])
+
+	.directive('pokemonName', [function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'pokedex/directives/partials/poke-name.tmpl.html'
+		};
+	}])
+
+	.directive('pokemonImage', [function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'pokedex/directives/partials/poke-image.tmpl.html'
 		};
 	}]);
 
