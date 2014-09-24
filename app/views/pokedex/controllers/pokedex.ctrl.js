a8c0a03ef08d6e0ce6d07b363f8d480912a147e7
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-09-24 10:39:16 +0200

    load name and images from pokedex

--- app/views/pokedex/controllers/pokedex.ctrl.js
+++ app/views/pokedex/controllers/pokedex.ctrl.js
@@ -1,6 +1,14 @@
 ( function(){
 	var PokedexController = function($scope, $http){
+		$scope.pokemons = [];
 
+		$http	.get('/data/pokemons.json')
+					.success(function(data){
+						$scope.pokemons = data;
+					})
+					.error(function(err){
+						console.log(err);
+					});
 	};
 
 	angular.module('pokeBoxApp.pokedex.controllers', [])
