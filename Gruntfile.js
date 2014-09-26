2c395e6106972e934033802c03077f0deea42b45
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-09-26 08:42:58 +0200

    sass livereload by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
-    connect: {
+    connect: { // create node JS port to simple web servise
       server: {
         options: {
           port: 9000,
@@ -9,7 +9,18 @@ module.exports = function (grunt) {
         }
       }
     },
-    watch: {
+     sass: { // deployment with sass
+      dist: {
+        files: {
+          'app/styles/app.css' : 'app/styles/app.scss'
+        }
+      }
+    },
+    watch: { // livereload ,no more F5
+      css:{
+        files: 'app/**/*.scss',
+        tasks: ['sass']
+      },
       project: {
         files: ['app/**/*.js', 'app/**/*.html', 'app/**/*.json', 'app/**/*.css'],
         options: {
@@ -21,7 +32,7 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['connect', 'watch', 'sass']);
 
 };
