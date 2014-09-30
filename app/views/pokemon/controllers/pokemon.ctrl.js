b7b85b2765c461b4e1a65b6077994f62d4012ec4
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-09-30 23:18:49 +0200

    refactor tabs controller

--- app/views/pokemon/controllers/pokemon.ctrl.js
+++ app/views/pokemon/controllers/pokemon.ctrl.js
@@ -1,13 +1,18 @@
 (function(){
-	var PokemonController = function($scope, $rootScope, $routeParams, PokeFact){
+	var PokemonController = function($scope, $rootScope, $routeParams, $location, PokeFact){
 		var	urlImages = "images/pokedex/",
 				pokeName = $routeParams.name;
 
 		$rootScope.appTitle= pokeName+" stats, abilities and evolutions";
 		$scope.pokemon = {};
-		PokeFact.getSinglePokeName( pokeName ).then(function(data){
-			$scope.pokemon = data; // when $q rejected the promise, then() never executed
-		});
+
+		PokeFact.getSinglePokeName( pokeName )
+			.then(function (data){
+				$scope.pokemon = data; // when $q rejected the promise, then() never executed
+			})
+			.catch(function(){
+				$rootScope.appTitle="pokemon not found";
+			});
 
 		$scope.getUrlImg = function( pokeId ){
 			return urlImages+pokeId;
@@ -20,11 +25,15 @@
 		$scope.selecTab = function( tab ){
 			$scope.tab = tab;
 		};
+
+		$scope.isActive = function( tab ){
+			return ( $scope.tab === tab ) && 'active';
+		};
 	};
 
 	angular.module('pokeBoxApp.pokemon.controllers', [])
 
-	.controller('PokemonCtrl', ['$scope', '$rootScope', '$routeParams', 'PokeFact', PokemonController])
+	.controller('PokemonCtrl', ['$scope', '$rootScope', '$routeParams', '$location', 'PokeFact', PokemonController])
 
 	.controller('TabsCtrl', ['$scope', TabsController]);
 }());
