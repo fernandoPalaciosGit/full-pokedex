63478d7be4a713a90043b9a1eeac3bc4bf336dbf
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-10-01 09:50:53 +0200

    search model to pokedex view

--- app/views/pokedex/controllers/pokedex.ctrl.js
+++ app/views/pokedex/controllers/pokedex.ctrl.js
@@ -1,37 +1,53 @@
 ( function(_){
-	var PokedexController = function($scope, $rootScope, $routeParams, PokeFact){
+	var PokedexController = function($scope, $rootScope, $routeParams, $filter, PokeFact){
 		var	pokeType = $routeParams.type,
 		
-				partition = function(data, n){
+				partition = function (data, n){
 					return _.chain(data).groupBy(function (element, index) {
 						return Math.floor(index / n);
 					}).toArray().value();
+				},
+				loadPokemon = function (arrPokemon){
+					$scope.filterPokemon = arrPokemon;	// show length of pokemon filtered
+					$scope.groupped = partition(arrPokemon, 4); // ng-repeat pokeomn filtered on view
 				};
 
 		$rootScope.appTitle= "full api collecion of pokedex national";
 		$scope.pokemons = [];
+		$scope.filterPokemon = [];
 		$scope.groupped = {};
+		$scope.searchTerm = '';
+
+		$scope.$watch('searchTerm', function(newValue, oldValue) {
+			if( !!newValue ){
+				var filterPokemon = $filter('filter')($scope.pokemons, {name : $scope.searchTerm});
+				loadPokemon( filterPokemon );
+			
+			} else {
+				loadPokemon( $scope.pokemons );
+			}
+		});
 
 		// check routeParam redirect
 		if( !!pokeType ){
 			$scope.type = pokeType;
-
+			
 			PokeFact.getSinglePokeType( pokeType )
 				.then(function (data){
 					$scope.pokemons = data;
-					$scope.groupped = partition(data, 4);
+					loadPokemon( data );
 				});
 
 		} else {
 			PokeFact.getAllPokemon()
 				.then(function (data){
 					$scope.pokemons = data;
-					$scope.groupped = partition(data, 4);
+					loadPokemon( data );
 				});
 		}
 	};
 
 	angular.module('pokeBoxApp.pokedex.controllers', [])
 
-	.controller('PokedexCtrl', ['$scope', '$rootScope', '$routeParams', 'PokeFact', PokedexController]);
+	.controller('PokedexCtrl', ['$scope', '$rootScope', '$routeParams', '$filter', 'PokeFact', PokedexController]);
 } (_) );
